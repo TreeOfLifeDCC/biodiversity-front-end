c5a649c94c13095360e4743ae2ecf915b6941588
Alexey Sokolov <alexey@ebi.ac.uk>
2023-11-30 10:29:06 +0000

    Adding gdpr banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,45 @@ import {GisComponent} from "./gis/gis.component";
 import {MatRadioModule} from "@angular/material/radio";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {FooterComponent} from "./footer/footer.component";
+import {NgcCookieConsentModule, NgcCookieConsentConfig} from 'ngx-cookieconsent';
+
+const cookieConfig:NgcCookieConsentConfig = {
+    "cookie": {
+        "domain": "www.ebi.ac.uk/biodiversity/"
+    },
+    "position": "bottom",
+    "theme": "classic",
+    "palette": {
+        "popup": {
+            "background": "#333",
+            "text": "#ffffff",
+            "link": "#ffffff"
+        },
+        "button": {
+            "background": "#f1d600",
+            "text": "#333",
+            "border": "transparent"
+        }
+    },
+    "type": "info",
+    elements:{
+        messagelink: `<span id="cookieconsent:desc" class="cc-message">{{message}} 
+                        <a aria-label="learn more about our privacy policy" tabindex="1" class="cc-link" href="{{privacyPolicyHref}}" target="_blank" rel="noopener">{{privacyPolicyLink}}</a> and  
+                        <a aria-label="learn more about our terms of service" tabindex="2" class="cc-link" href="{{tosHref}}" target="_blank" rel="noopener">{{tosLink}}</a>
+                    </span>
+                    `,
+    },
+    "content": {
+        "message": "This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our ",
+        "dismiss": "I agree, dismiss this banner",
+
+        privacyPolicyLink: 'Privacy Notice',
+        privacyPolicyHref: 'https://www.ebi.ac.uk/data-protection/privacy-notice/embl-ebi-public-website',
+
+        tosLink: 'Terms of Use',
+        tosHref: 'https://www.ebi.ac.uk/about/terms-of-use',
+    }
+};
 
 
 
@@ -91,8 +130,7 @@ import {FooterComponent} from "./footer/footer.component";
         MatTableExporterModule,
         MatRadioModule,
         MatAutocompleteModule,
-
-
+        NgcCookieConsentModule.forRoot(cookieConfig)
     ],
     providers: [],
     bootstrap: [AppComponent]
